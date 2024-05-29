83dd90c5d620224212598ec89bebb78b3f6b3949
ShaYri12 <sharjeelb60@gmail.com>
2024-05-29 23:11:52 +0500

    fix little styling

--- app/components/Partners.js
+++ app/components/Partners.js
@@ -15,19 +15,19 @@ const Partners = () => {
             <h2 className="text-4xl font-extrabold text-white text-center mb-8">Our Partners</h2>
             <Marquee {...settings}  direction="right">
               <div className="flex justify-between">
-                <Link href="https://securitybot.gg/" target="_blank" className="w-[350px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
+                <Link href="https://securitybot.gg/" target="_blank" className="w-[320px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
                     <img src="/assets/security_logo.png" alt="Partner 1" className="h-32 w-32 object-contain  rounded-lg"/>
                     <span className='text-white text-lg'>Security Bot</span>
                 </Link>
-                <Link href="https://erisly.com/" target="_blank" className="w-[350px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
+                <Link href="https://erisly.com/" target="_blank" className="w-[320px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
                     <img src="/assets/erisly_logo.png" alt="Partner 2" className="h-32 w-32 object-contain rounded-lg"/>
                     <span className='text-white text-lg'>Erisly</span>
                 </Link>
-                <Link href="https://studiofow.com/" target="_blank" className="w-[350px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
+                <Link href="https://studiofow.com/" target="_blank" className="w-[320px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
                     <img src="/assets/fow_logo.png" alt="Partner 3" className="h-32 w-32 object-contain rounded-lg"/>
                     <span className='text-white text-lg'>Studio FOW</span>
                 </Link>
-                <Link href="https://discords.com/" target="_blank" className="w-[350px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
+                <Link href="https://discords.com/" target="_blank" className="w-[320px] flex flex-col items-center justify-center rounded-lg p-4 space-y-2">
                     <img src="/assets/discords_logo.png" alt="Partner 4" className="h-32 w-32 object-contain rounded-lg"/>
                     <span className='text-white text-lg'>Discords</span>
                 </Link>
--- app/components/Stats.js
+++ app/components/Stats.js
@@ -7,18 +7,18 @@ const Stats = () => {
   return (
     <div className="bg-bg_footer text-white py-8 pt-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
-          <div className="flex flex-col items-center">
+        <div className="flex flex-wrap  justify-between text-center">
+          <div className="flex flex-col p-3 items-center flex-grow">
             <FaServer size={50} />
             <div className="text-2xl font-bold mt-2">+2.7M</div>
             <div className="text-lg font-bold">Servers</div>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col p-3 items-center flex-grow">
             <MdOutlineTimer size={50} />
             <div className="text-2xl font-bold mt-2">+32</div>
             <div className="text-lg font-bold">Counters</div>
           </div>
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col p-3 items-center flex-grow">
             <HiOutlineCommandLine size={50} />
             <div className="text-2xl font-bold mt-2">+30</div>
             <div className="text-lg font-bold">Commands</div>
