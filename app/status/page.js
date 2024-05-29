c4a71be2dcf6183d8ac3f87e4a3f359112a42b78
ShaYri12 <sharjeelb60@gmail.com>
2024-05-30 02:13:40 +0500

    status page almost complete

--- app/status/page.js
+++ app/status/page.js
@@ -1,13 +1,178 @@
-'use client'
-import React, { useEffect } from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Tooltip, Button, Input } from "@material-tailwind/react";
+import { FaSearch } from "react-icons/fa";
 
 const Status = () => {
+    const [data, setData] = useState(null);
+    const [nextUpdate, setNextUpdate] = useState(60); // Initial countdown until next update
+    const [searchTerm, setSearchTerm] = useState("");
+    
     useEffect(() => {
         window.scrollTo(0, -1);
-      }, []);
-    return (
-        <div>Status</div>
-    )
+    }, []);
+
+  useEffect(() => {
+    fetchData(); // Fetch data initially
+    const intervalId = setInterval(fetchData, 60000); // Fetch data every 60 seconds
+
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const fetchData = () => {
+    // Fetch the data from the API
+    fetch('https://api.serverstatsbot.com/stats') // replace with your actual API endpoint
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        setNextUpdate(60); // Reset countdown after each successful fetch
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
+  useEffect(() => {
+    // Update countdown every second
+    const intervalId = setInterval(() => {
+      setNextUpdate(prevTime => (prevTime > 0 ? prevTime - 1 : 60)); // Decrease countdown every second
+    }, 1000);
+
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+    
+      const formatUptime = (seconds) => {
+        const days = Math.floor(seconds / (24 * 3600));
+        const remainingSecondsAfterDays = seconds % (24 * 3600);
+        const hours = Math.floor(remainingSecondsAfterDays / 3600);
+        const remainingSecondsAfterHours = remainingSecondsAfterDays % 3600;
+        const minutes = Math.floor(remainingSecondsAfterHours / 60);
+        
+        const formattedUptime = [];
+      
+        if (days > 0) formattedUptime.push(`${days}d`);
+        if (hours > 0) formattedUptime.push(`${hours}h`);
+        if (minutes > 0 || (days === 0 && hours === 0)) formattedUptime.push(`${minutes}m`);
+      
+        return formattedUptime.join(', ');
+      };
+
+  if (!data) {
+    return <div className='flex items-center justify-center min-h-screen font-bold text-white text-lg'>Loading...</div>;
+  }
+
+  return (
+    <section className='py-24'>
+      <div className="bg-zinc-900 min-h-screen p-6 text-white">
+        <div className="max-w-4xl mx-auto">
+          <h1 className="text-4xl font-bold mb-4">Bot Status</h1>
+          <div className="mb-6 space-y-1">
+            <p>Next update: <span className="font-semibold">{nextUpdate}</span></p>
+            <p>Counter Updates: <span className="font-semibold">{data.totalCounterUpdates}</span></p>
+            <p>Unavailable Servers: <span className="font-semibold">{data.totalUnavailableGuilds}</span></p>
+          </div>
+            <div className="flex md:w-96 w-auto text-white mb-6">
+                <Input 
+                    color="light-blue" 
+                    className='text-white rounded-none rounded-l-lg' 
+                    variant="outlined" 
+                    label="Search for a server by ID"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                <Button className="bg-blue-500 text-white px-4 rounded-none rounded-r-lg"><FaSearch size="16"/></Button>
+            </div>
+          <div className="flex items-center flex-wrap mb-6">
+            <div className="flex items-center p-2">
+              <span className="w-4 h-4 bg-green-400 rounded-full inline-block mr-2"></span>
+              <span>Online</span>
+            </div>
+            <div className="flex items-center p-2">
+              <span className="w-4 h-4 bg-yellow-700 rounded-full inline-block mr-2"></span>
+              <span>Partial Outage</span>
+            </div>
+            <div className="flex items-center p-2">
+              <span className="w-4 h-4 bg-red-600 rounded-full inline-block mr-2"></span>
+              <span>Outage</span>
+            </div>
+            <div className="flex items-center p-2">
+              <span className="w-4 h-4 bg-gray-600 rounded-full inline-block mr-2"></span>
+              <span>Offline</span>
+            </div>
+          </div>
+          <div className="grid grid-cols-5 sm:grid-cols-7 md:grid-cols-10 gap-2">
+            {data.clusters.map(cluster => (
+              <Tooltip className="bg-gray-800/95"
+                key={cluster.cluster}
+                content={(
+                    
+                  <div className="p-2">
+                    {cluster.shardsStats.map(shard => (
+                      <div
+                        key={shard.id}
+                        className={`p-1 rounded-lg text-left  ${getStatusClass(shard.status)}`}
+                      >
+                        {`Shard ${shard.id} - ${shard.latency}ms`}
+                      </div>
+                    ))}
+                    <hr className='my-2'/>
+                    <div className="p-1">
+                    <p><span>Cluster Uptime:</span> {formatUptime(cluster.clusterUptime)}</p>
+                      <p><span>Guilds:</span> {cluster.guilds}</p>
+                    </div>
+                  </div>
+                )}
+                animate={{
+                  mount: { scale: 1, y: 0 },
+                  unmount: { scale: 0, y: 25 },
+                }}
+                placement="left"
+              >
+                <Button
+                  className={`md:p-4 p-2 rounded-lg text-center text-lg font-semibold ${getClusterStatusClass(cluster.shardsStats)}`}
+                >
+                  {cluster.cluster + 1}
+                </Button>
+              </Tooltip>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+);
 }
 
-export default Status
+export default Status;
+
+function getClusterStatusClass(shardsStats) {
+const allStatuses = shardsStats.map(shard => shard.status);
+const allReady = allStatuses.every(status => status === 'ready');
+const allOffline = allStatuses.every(status => status === 'offline');
+const someReady = allStatuses.some(status => status === 'ready');
+const someOffline = allStatuses.some(status => status === 'offline');
+
+if (allReady) return 'text-green-400';
+if (allOffline) return 'text-gray-600';
+if (someReady && someOffline) return 'text-yellow-700';
+if (someReady) return 'text-yellow-500'; // Adjust this based on your needs
+return 'text-zinc-800'; // Default case if none of the conditions match
+}
+
+function getStatusClass(status) {
+switch (status) {
+  case 'ready':
+    return 'text-green-400';
+  case 'partial':
+    return 'text-yellow-700';
+  case 'outage':
+    return 'text-red-600';
+  case 'offline':
+    return 'text-gray-600';
+  default:
+    return '';
+}
+}
