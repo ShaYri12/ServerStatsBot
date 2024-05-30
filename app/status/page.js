e08460e1ec57b8e8973a1632389b6ef301c6e4bb
ShaYri12 <sharjeelb60@gmail.com>
2024-05-31 03:00:03 +0500

    added server id search

--- app/status/page.js
+++ app/status/page.js
@@ -4,14 +4,14 @@ import { Tooltip, Button, Input } from "@material-tailwind/react";
 import { FaSearch } from "react-icons/fa";
 
 const Status = () => {
-    const [data, setData] = useState(null);
-    const [nextUpdate, setNextUpdate] = useState(60); // Initial countdown until next update
-    const [searchTerm, setSearchTerm] = useState("");
-    const [searchResult, setSearchResult] = useState(null);
+  const [data, setData] = useState(null);
+  const [nextUpdate, setNextUpdate] = useState(60); // Initial countdown until next update
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState(null);
 
-    useEffect(() => {
-        window.scrollTo(0, -1);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, -1);
+  }, []);
 
   useEffect(() => {
     fetchData(); // Fetch data initially
@@ -48,20 +48,18 @@ const Status = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
-    const searched = searchTerm;
-    const result = data.clusters.find(cluster => {
-        return cluster.shardsStats.some(shard => shard.id.toString() === searched);
-    });
+    const totalShards = data.clusters.length * 16;
+    const serverId = parseInt(searchTerm, 10);
+    const shardId = Math.floor((serverId / 4194304) % totalShards);
+    const cluster = data.clusters.find(cluster => cluster.shardsStats.some(shard => shard.id === shardId));
 
-    if (result) {
-        // Assuming shardID is a property of the shard object inside the result
-        const shardID = result.shardsStats.find(shard => shard.id.toString() === searched)?.id;
-        setSearchResult({ ...result, shardID: shardID });
+    if (cluster) {
+      setSearchResult({ cluster: cluster.cluster, shardID: shardId });
     } else {
-        setSearchResult(null);
+      setSearchResult(null);
     }
-};
-    
+  };
+
   const formatUptime = (totalMilliseconds) => {
     const totalSeconds = Math.floor(totalMilliseconds / 1000);
     const days = Math.floor(totalSeconds / (60 * 60 * 24));
@@ -71,17 +69,17 @@ const Status = () => {
     const formattedTime = [];
 
     if (days > 0) {
-        formattedTime.push(`${days}d`);
+      formattedTime.push(`${days}d`);
     }
     if (hours > 0) {
-        formattedTime.push(`${hours}h`);
+      formattedTime.push(`${hours}h`);
     }
     if (minutes > 0 || days === 0 && hours === 0) {
-        formattedTime.push(`${minutes}m`);
+      formattedTime.push(`${minutes}m`);
     }
 
     return formattedTime.join(' ');
-};
+  };
 
   if (!data) {
     return <div className='flex items-center justify-center min-h-screen font-bold text-white text-lg'>Loading...</div>;
@@ -97,26 +95,25 @@ const Status = () => {
             <p>Counter Updates: <span className="font-semibold">{data.totalCounterUpdates}</span></p>
             <p>Unavailable Servers: <span className="font-semibold">{data.totalUnavailableGuilds}</span></p>
           </div>
-            <form className="flex md:w-96 w-auto text-white mb-6" onSubmit={handleSearch}>
-                <Input 
-                    color="light-blue" 
-                    className='text-white rounded-none rounded-l-lg' 
-                    type="number"
-                    variant="outlined" 
-                    label="Search for a server by ID"
-                    value={searchTerm}
-                    onChange={handleSearchChange}
-                    
-                />
-                <Button onClick={handleSearch} className="bg-blue-500 text-white px-4 rounded-none rounded-r-lg"><FaSearch size="16"/></Button>
-            </form>
-            {searchResult && (
-                <div className="mb-4">
-                    <p className="text-lg text-white">
-                    * Located in cluster {searchResult.cluster + 1} and shard {searchResult.shardID} *
-                    </p>
-                </div>
-            )}
+          <form className="flex md:w-96 w-auto text-white mb-6" onSubmit={handleSearch}>
+            <Input
+              color="light-blue"
+              className='text-white rounded-none rounded-l-lg'
+              type="number"
+              variant="outlined"
+              label="Search for a server by ID"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+            <Button onClick={handleSearch} className="bg-blue-500 text-white px-4 rounded-none rounded-r-lg"><FaSearch size="16" /></Button>
+          </form>
+          {searchResult && (
+            <div className="mb-4">
+              <p className="text-lg text-white">
+                * Located in cluster {searchResult.cluster + 1} and shard {searchResult.shardID} *
+              </p>
+            </div>
+          )}
           <div className="flex items-center flex-wrap mb-6">
             <div className="flex items-center p-2">
               <span className="w-4 h-4 bg-green-400 rounded-full inline-block mr-2"></span>
@@ -140,20 +137,19 @@ const Status = () => {
               <Tooltip className="bg-gray-800/95"
                 key={cluster.cluster}
                 content={(
-                    
                   <div className="p-2">
                     {cluster.shardsStats.map(shard => (
                       <div
                         key={shard.id}
-                        className={`p-1 rounded-lg text-left  ${getStatusClass(shard.status)}`}
+                        className={`p-1 rounded-lg text-left ${getStatusClass(shard.status)}`}
                       >
                         {`Shard ${shard.id} - ${shard.latency}ms`}
                       </div>
                     ))}
-                    <hr className='my-2'/>
+                    <hr className='my-2' />
                     <div className="p-1 space-y-1">
-                        <p><span>Uptime:</span> {formatUptime(cluster.clusterUptime)}</p>
-                        <p><span>Guilds:</span> {cluster.guilds}</p>
+                      <p><span>Uptime:</span> {formatUptime(cluster.clusterUptime)}</p>
+                      <p><span>Guilds:</span> {cluster.guilds}</p>
                     </div>
                   </div>
                 )}
@@ -174,36 +170,36 @@ const Status = () => {
         </div>
       </div>
     </section>
-);
+  );
 }
 
 export default Status;
 
 function getClusterStatusClass(shardsStats) {
-const allStatuses = shardsStats.map(shard => shard.status);
-const allReady = allStatuses.every(status => status === 'ready');
-const allOffline = allStatuses.every(status => status === 'offline');
-const someReady = allStatuses.some(status => status === 'ready');
-const someOffline = allStatuses.some(status => status === 'offline');
+  const allStatuses = shardsStats.map(shard => shard.status);
+  const allReady = allStatuses.every(status => status === 'ready');
+  const allOffline = allStatuses.every(status => status === 'offline');
+  const someReady = allStatuses.some(status => status === 'ready');
+  const someOffline = allStatuses.some(status => status === 'offline');
 
-if (allReady) return 'text-green-400';
-if (allOffline) return 'text-gray-600';
-if (someReady && someOffline) return 'text-yellow-700';
-if (someReady) return 'text-yellow-500'; // Adjust this based on your needs
-return 'text-zinc-800'; // Default case if none of the conditions match
+  if (allReady) return 'text-green-400';
+  if (allOffline) return 'text-gray-600';
+  if (someReady && someOffline) return 'text-yellow-700';
+  if (someReady) return 'text-yellow-500'; // Adjust this based on your needs
+  return 'text-zinc-800'; // Default case if none of the conditions match
 }
 
 function getStatusClass(status) {
-switch (status) {
-  case 'ready':
-    return 'text-green-400';
-  case 'partial':
-    return 'text-yellow-700';
-  case 'outage':
-    return 'text-red-600';
-  case 'offline':
-    return 'text-gray-600';
-  default:
-    return '';
-}
+  switch (status) {
+    case 'ready':
+      return 'text-green-400';
+    case 'partial':
+      return 'text-yellow-700';
+    case 'outage':
+      return 'text-red-600';
+    case 'offline':
+      return 'text-gray-600';
+    default:
+      return '';
+  }
 }
